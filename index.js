30ac001df9874f13871a71b999abec77a22b1709
Jason Bracco <bracco.jason@gmail.com>
2022-04-28 19:58:24 -0400

    commit tree dc62e039787f36b32afd4795482cfe91317172b5

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(activity = "go to the office"){
+        return `This Monday, I will ${activity}.`
+    }
+
+function wrapAdjective(flair = '*'){
+    const innerFunction = function(ayo= "special") {
+        return `You are ${flair}${ayo}${flair}!`
+    }
+    return innerFunction
+}
+//const stored = wrapAdjective(flair = '*')
+//stored(ayo = "special")
